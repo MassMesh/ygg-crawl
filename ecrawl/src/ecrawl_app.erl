9c37784164f1626d81cf7c2e5f3e53c46d2d9f61
Rowan Cannaday <cannadayr@gmail.com>
2019-12-17 18:04:22 -0500

    add init fn & export

--- ecrawl/src/ecrawl_app.erl
+++ ecrawl/src/ecrawl_app.erl
@@ -7,12 +7,16 @@
 
 -behaviour(application).
 
--export([start/2, stop/1]).
+-export([start/2, stop/1, init/0]).
 
 start(_StartType, _StartArgs) ->
+    %init(),
     ecrawl_sup:start_link().
 
 stop(_State) ->
     ok.
 
 %% internal functions
+init() ->
+    io:format("hello!\n"),
+    ok.
